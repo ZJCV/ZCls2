33de745bc6ab4fdedb07754c075cec13a7ce16be
zjykzj <wy163zhuj@163.com>
2022-04-14 14:20:46 +0800

    test(optim): test CosineAnnealingLR and MultiStepLR

---
+++ test/optim/lr_scheduler/multistep_lr.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/4/14 上午11:25
+@file: cosine_annealing_lr.py
+@author: zj
+@description: 
+"""
+
+import torch
+
+from zcls2.config import cfg
+from zcls2.model.model.build import build_model
+from zcls2.optim.optimizer.build import build_optimizer
+from zcls2.optim.lr_scheduler.build import build_lr_scheduler
+
+
+def ca():
+    config_file = 'configs/mslr.yaml'
+    cfg.merge_from_file(config_file)
+    # print(cfg)
+
+    model = build_model(cfg, torch.contiguous_format)
+    optimizer = build_optimizer(cfg, model)
+    lr_scheduler = build_lr_scheduler(cfg, optimizer)
+
+    max_epoch = cfg.TRAIN.MAX_EPOCH
+    for i in range(max_epoch):
+        print(i, optimizer.param_groups[0]['lr'])
+        optimizer.step()
+        lr_scheduler.step()
+
+
+if __name__ == '__main__':
+    ca()
