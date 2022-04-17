1947713bb57df6a72744cc3f6193244a463c611c
zjkjzj <wy163zhuj@163.com>
2022-04-17 15:48:03 +0800

    perf(train.sh): update usage

--- tools/train.sh
+++ tools/train.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -eux
+
+# Usage: bash tools/train.sh <config-file path>
+
+echo "Execution file name: $0"
+echo "The first param: $1"
+
 export PYTHONPATH=.
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port="15231" \
-  tools/train.py -cfg configs/benchmarks/r18_cifar100_224_b246_e90_g4.yaml \
+  tools/train.py -cfg $1 \
   --opt-level O1
