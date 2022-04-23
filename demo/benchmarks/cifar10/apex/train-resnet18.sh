ef9df4ec7f10627600f18f146108d3a2de24a53f
zjkjzj <wy163zhuj@163.com>
2022-04-23 17:33:29 +0800

    perf(benchmarks): change lr from 0.1 to 0.01. Make better accuracy

--- demo/benchmarks/cifar10/apex/train-resnet18.sh
+++ demo/benchmarks/cifar10/apex/train-resnet18.sh
@@ -21,7 +21,7 @@ echo "start time: $time1"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port="15231" \
   ${train_sh} --arch ${arch} --pretrained --num-classes ${num_classes} \
-  --workers 4 --batch-size 256 --learning-rate 0.1 --momentum 0.9 --weight-decay 1e-4 \
+  --workers 4 --batch-size 256 --learning-rate 0.01 --momentum 0.9 --weight-decay 1e-4 \
   --epochs 90 --start-epoch 0 \
   --opt-level O1 \
   "${folder}/${dataset_name}"
