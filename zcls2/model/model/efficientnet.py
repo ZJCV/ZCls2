95fda50db3695c6ba5a004855cc3598f5ec034e1
zjkjzj <wy163zhuj@163.com>
2022-05-04 23:18:14 +0800

    style(efficientnet.py): update description

--- zcls2/model/model/efficientnet.py
+++ zcls2/model/model/efficientnet.py
@@ -5,6 +5,17 @@
 @file: efficientnet.py
 @author: zj
 @description: Custom EfficientNet, derived from torchvision
+
+        'efficientnet-b0': (1.0, 1.0, 224, 0.2),
+        'efficientnet-b1': (1.0, 1.1, 240, 0.2),
+        'efficientnet-b2': (1.1, 1.2, 260, 0.3),
+        'efficientnet-b3': (1.2, 1.4, 300, 0.3),
+        'efficientnet-b4': (1.4, 1.8, 380, 0.4),
+        'efficientnet-b5': (1.6, 2.2, 456, 0.4),
+        'efficientnet-b6': (1.8, 2.6, 528, 0.5),
+        'efficientnet-b7': (2.0, 3.1, 600, 0.5),
+        'efficientnet-b8': (2.2, 3.6, 672, 0.5),
+        'efficientnet-l2': (4.3, 5.3, 800, 0.5),
 """
 
 from functools import partial
