03050ebcde1ba62724bc162d0ad624d070194dc2
zjkjzj <wy163zhuj@163.com>
2022-07-26 22:30:39 +0800

    fix(setup.py): fix INSTALL_REQUIRES settings

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ INSTALL_REQUIRES = [
     "opencv_contrib_python >= 4.5.5.64",
     "tabulate >= 0.8.9",
     "timm >= 0.4.9",
-    "torch >= 1.12.0a0 + 2c916ef",
+    "torch >= 1.12.0a02c916ef",
     "torchvision >= 0.13.0a0",
     "yacs >= 0.1.8",
 ]
